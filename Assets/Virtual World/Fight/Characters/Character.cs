bc26748fdaa39b38b8bf34e008c278cfd1e9359d
luis M Ponce <luisponcekun@gmail.com>
2013-10-07 17:50:23 -0500

    Fix #22 - Implemented motion

--- Assets/Virtual World/Fight/Characters/Character.cs
+++ Assets/Virtual World/Fight/Characters/Character.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof (Motion))]
 public class Character : MonoBehaviour {
-
+	
+	private Vector2 moveDir;
+	private Motion motion;
+	
 	// Use this for initialization
 	void Start () {
-	
+		motion=gameObject.GetComponent<Motion>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKey("w")){
+			moveDir.y=1;
+		} else{
+			if(Input.GetKey("s")){
+				moveDir.y=-1;
+			} else {
+				moveDir.y=0;
+			}
+		}
+		
+		if (Input.GetKey("d")){
+			moveDir.x=1;
+		} else{
+			if(Input.GetKey("a")){
+				moveDir.x=-1;
+			} else {
+				moveDir.x=0;
+			}
+		}
+		
+		motion.Move(moveDir);
 	}
 }
---
+++ Assets/Virtual World/Fight/Characters/Motion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CharacterController))]
+public class Motion : MonoBehaviour {
+	public float speed = 200;
+	private CharacterController controller;
+	// Use this for initialization
+	void Start () {
+		controller = gameObject.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	/// <summary>
+	/// Move the specified movement.
+	/// </summary>
+	/// <param name='movement'>
+	/// Movement represented in a vector with both values clamped between -1 and 1
+	/// </param>
+	public void Move(Vector2 movement){
+		controller.SimpleMove(new Vector3(movement.x, 0, movement.y) * Time.deltaTime * speed);
+	}
+}
